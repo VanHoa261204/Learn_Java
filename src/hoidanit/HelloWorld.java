86a9f89d342bd66f5ed84e89ed789b7d613881ba
VanHoa261204 <144088536+VanHoa261204@users.noreply.github.com>
2026-03-17 12:51:04 +0700

    nhap tu ban phim

--- src/hoidanit/HelloWorld.java
+++ src/hoidanit/HelloWorld.java
@@ -1,7 +1,20 @@
 package hoidanit;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		int age;
+		String name;
+
+		System.out.printf("Name: ");
+		name = sc.nextLine();
+		System.out.printf("Tuoi: ");
+		age = sc.nextInt();
+
+		System.out.println("My name is " + name + ", i am " + age);
+		sc.close();
 	}
+
 }
